300087146b15f347804fb8b3d88546a3c1c92565
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-11-27 07:49:54 +0800

    Edited data.py

--- data.py
+++ data.py
@@ -3,10 +3,15 @@ class Data:
     def __init__(self):
         self.conn = sqlite3.connect("data.db")
         self.cur = self.conn.cursor()
-    def construct(self):
         self.cur.execute("""
-        CREATE TABLE IF NOT EXISTS data(
-            id INT AUTO_INCREMENT PRIMARY KEY
-            player_name TEXT
-            score INT
-        )""")
+                CREATE TABLE IF NOT EXISTS leaderboard(
+                    id INT AUTO_INCREMENT PRIMARY KEY
+                    player_name TEXT
+                    score INT
+                )""")
+
+    def add(self, name, score):
+        sql = "INSERT INTO leaderboard (player_name, score) VALUES (?, ?)"
+        val = (name, score)
+        self.cur.execute(sql, val)
+        self.conn.commit()
