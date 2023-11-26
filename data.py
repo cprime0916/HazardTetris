63c6a217f34c002ca3e56c640df7902127476ea9
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-11-26 14:37:28 +0800

    Edited database module file

--- data.py
+++ data.py
@@ -4,5 +4,9 @@ class Data:
         self.conn = sqlite3.connect("data.db")
         self.cur = self.conn.cursor()
     def construct(self):
-        self.cur.execute("CREATE TABLE IF NOT EXISTS")
-        pass
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS data(
+            id INT AUTO_INCREMENT PRIMARY KEY
+            player_name TEXT
+            score INT
+        )""")
