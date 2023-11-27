7db4325fa8638d3b18540117a30d2c2f9de5d6b1
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-11-28 07:53:16 +0800

    Edited data.py

--- data.py
+++ data.py
@@ -5,8 +5,8 @@ class Data:
         self.cur = self.conn.cursor()
         self.cur.execute("""
                 CREATE TABLE IF NOT EXISTS leaderboard(
-                    id INT AUTO_INCREMENT PRIMARY KEY
-                    player_name TEXT
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    player_name TEXT,
                     score INT
                 )""")
 
@@ -15,3 +15,9 @@ class Data:
         val = (name, score)
         self.cur.execute(sql, val)
         self.conn.commit()
+    def at(self, arg):
+        if(arg.isnum()):
+            sql = "SELECT player_name FROM leaderboard WHERE score = ?"
+            val = (arg,)
+            self.cur.execute(sql, val)
+            self.conn.commit()
