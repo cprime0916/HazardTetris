9549c5829e0256cfc6efafffd0ce8ccf8cb89e33
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-11-28 17:24:44 +0800

    Debugged draw func

--- ui.py
+++ ui.py
@@ -5,7 +5,7 @@ class Button:
         self.rect = pygame.Rect(x, y, 800, 600)
         self.text = text
     def draw(self):
-        pygame.draw.rect(screen, RED, self.rect)
+        pygame.draw.rect(screen, BLUE, self.rect)
         font = pygame.font.Font(None, 30)
         text = font.render(self.text, True, WHITE)
         text_rect = text.get_rect(center=self.rect.center)
