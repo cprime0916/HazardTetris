04e80df81629893d64c7b846b62e3eaa01bd6664
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-11-28 17:17:18 +0800

    Added ui.py

---
+++ ui.py
@@ -0,0 +1,12 @@
+import pygame
+from const import *
+class Button:
+    def __init__(self, x, y, text):
+        self.rect = pygame.Rect(x, y, 800, 600)
+        self.text = text
+    def draw(self):
+        pygame.draw.rect(screen, RED, self.rect)
+        font = pygame.font.Font(None, 30)
+        text = font.render(self.text, True, WHITE)
+        text_rect = text.get_rect(center=self.rect.center)
+        screen.blit(text, text_rect)
