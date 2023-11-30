babf2acb8971f1e6e9b40f41bb0ca5c51a0a49f5
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-11-30 22:35:50 +0800

    Edited game over to restart diff

--- main.py
+++ main.py
@@ -188,7 +188,8 @@ def main(diff):
             if event.type == pygame.KEYDOWN:
                 # Only Button R resets the game
                 if event.key == pygame.K_r:
-                    game = Tetris(WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE)
+                    start_menu_main()
+                    # game = Tetris(WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE)
             # Check for the KEYDOWN event
         # Update the display
         pygame.display.flip()
